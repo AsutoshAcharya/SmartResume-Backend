330d4980c30713ca71f35575ac20bee4f136fc81
Asutosh Acharya <asutosh246@gmail.com>
2025-05-22 23:56:19 +0530

    added resume add controller

--- src/resume/resume.controller.ts
+++ src/resume/resume.controller.ts
@@ -1,16 +1,27 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { ResumeService } from './resume.service';
 import { AuthGuard } from '@nestjs/passport';
 import { JwtDto } from 'src/user/dto/user.dto';
+import { CreateResumeDto } from './dto/resume.dto';
 
 @Controller('resume')
 export class ResumeController {
   constructor(private resumeService: ResumeService) {}
   @UseGuards(AuthGuard('jwt'))
   @Post('add')
-  createResume(@Request() req: Request & { user: JwtDto }) {
-    console.log(req?.user);
-    return `you can now add`;
+  createResume(
+    @Request() req: Request & { user: JwtDto },
+    @Body() createResumeDto: CreateResumeDto,
+  ) {
+    // console.log(req?.user);
+    return this.resumeService.createResume(req?.user?.id, createResumeDto);
   }
 
   @UseGuards(AuthGuard('jwt'))
